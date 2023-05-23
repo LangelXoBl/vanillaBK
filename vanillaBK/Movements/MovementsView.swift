912f19e7fcc3dd46e5065c1e671730c935827481
Langelxobl <langelxobl@gmail.com>
2023-05-22 20:01:55 -0500

    Lista de movimientos v1

---
+++ vanillaBK/Movements/MovementsView.swift
@@ -0,0 +1,88 @@
+//
+//  MovementsView.swift
+//  vanillaBK
+//
+//  Created by IMAC 03 on 22/05/23.
+//
+
+import SwiftUI
+
+struct movements: Identifiable {
+    let id = UUID()
+    var monto: Int
+    var fecha: String
+    var concepto: String
+    var icon: Image
+    var status: Int //0 pendiete | 1 completado
+}
+
+struct Movement: View{
+    var movement = movements(monto: 100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 0)
+   
+    var body: some View{
+        HStack{
+            movement.icon
+            VStack{
+                Text(movement.fecha)
+                Text(movement.concepto)
+            }
+            Spacer()
+            VStack{
+                Text("$"+String( movement.monto))
+                Text(movement.status==0 ?"Pendiente":"Completado")
+            }
+        }.background(.gray).padding(.horizontal,40)
+    }
+}
+
+struct MovementsView: View {
+    var data=[
+        movements(monto: 100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 0),movements(monto: -100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1),movements(monto: 100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 0),
+        movements(monto: -1200, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1),
+        movements(monto: -100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1),movements(monto: 100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 0),
+        movements(monto: -1200, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1)
+        ,movements(monto: -100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1),movements(monto: 100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 0),
+        movements(monto: -1200, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1),
+        movements(monto: -100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1),movements(monto: 100, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 0),
+        movements(monto: -1200, fecha: "2023-02-13", concepto: "compra de picsa", icon: Image(systemName: "checkmark.circle.fill"), status: 1)
+    ]
+    @State private var search:
+    String = ""
+    var body: some View {
+        VStack{
+            //titulo
+            Text("Movimientos").font(.title)
+            
+            //Buscador
+            HStack{
+                TextField("Buscar por concepto",text:
+                            $search).font(.caption).padding(10).border(.black)
+                Image(systemName: "magnifyingglass")
+            }.padding(.horizontal,40)
+            
+            //lista
+            List(data){movement in
+                HStack{
+                    movement.icon
+                    VStack{
+                        Text(movement.fecha)
+                        Text(movement.concepto)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("$"+String( movement.monto))
+                        Text(movement.status==0 ?"Pendiente":"Completado")
+                    }
+                }.padding(10)
+                .background(.gray.opacity(0.25))
+            }.scrollContentBackground(.hidden)
+        }
+    }
+}
+
+struct MovementsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovementsView()
+        //Movement()
+    }
+}
