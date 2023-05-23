e32a3a5dcf2a47d005938d26d26bac06c0ae1df2
Langelxobl <langelxobl@gmail.com>
2023-05-22 19:53:33 -0500

    Initial Commit

---
+++ vanillaBK/vanillaBKApp.swift
@@ -0,0 +1,17 @@
+//
+//  vanillaBKApp.swift
+//  vanillaBK
+//
+//  Created by IMAC 03 on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct vanillaBKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
