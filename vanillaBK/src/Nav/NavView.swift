608a332479fa52ee23daaf806bed8d88dbf72f6b
Luis Angel xool balam <langelxobl@gmail.com>
2023-06-12 18:23:12 -0500

    fix nav
    add dashboard al nav

--- vanillaBK/src/Nav/NavView.swift
+++ vanillaBK/src/Nav/NavView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct NavView: View {
     var body: some View {
         TabView{
+            Dashboardview().tabItem{
+                Image(systemName: "1.circle")
+                Text("Dashboard")
+            }
             MovementsView().tabItem{
                 Image(systemName: "2.circle")
                 Text("movimeinto")
             }
             TransferView().tabItem{
-                Image(systemName: "1.circle")
+                Image(systemName: "3.circle")
                 Text("movimeinto")
             }
         }
